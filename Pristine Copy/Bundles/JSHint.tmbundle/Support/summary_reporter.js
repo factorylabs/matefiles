fc57ac0226de3e3b734cd99b9e66f855d45e40a9
Matthew Kitt <mk.kitt@gmail.com>
2011-03-27 11:11:25 -0600

    Adding JSHint validation bundle

---
+++ Pristine Copy/Bundles/JSHint.tmbundle/Support/summary_reporter.js
@@ -0,0 +1,47 @@
+/**
+ * Outputs to a TextMate HTML window printing JSLint results.
+ * Invoked by "⌃⇧V"
+ * @author Matthew Kitt
+ */
+var sys = require('sys');
+
+function reporter(results) {
+  var error_regexp = /^\s*(\S*(\s+\S+)*)\s*$/,
+      i,
+      len = results.length,
+      output = '',
+      html = '',
+      file,
+      error,
+      reason,
+      line,
+      character;
+
+  for (i = 0; i < len; i += 1) {
+    file = results[i].file;
+    error = results[i].error;
+    reason = error.reason;
+    line = error.line;
+    character = error.character;
+    output += '<li>' + 
+                '<a href="txmt://open?url=file://' + file + '&line=' + 
+                                line + '&column=' + character + '">' +
+                  '<strong>' + reason + '</strong>' +
+                  ' <em>line ' + line + 
+                  ', col ' + character + '</em>' +
+                '</a>' +
+                '<pre><code>' + 
+                  (error.evidence || '').replace(error_regexp, "$1") +
+                '</code></pre>' +
+              '</li>';
+  }
+  
+  html += '<h1>' + len + ' Error' + ((len === 1) ? '' : 's') + '</h1>' +
+          '<hr/>' +
+          '<ol>' +
+            output +
+          '</ol>';
+
+  sys.puts(html);
+  sys.puts('');
+}
